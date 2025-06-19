ae16a480341decf3f5fbf401a89e8b68a3d0c01f
Sergio <raidanearth@gmail.com>
2025-06-19 11:53:55 -0300

    Obtener productos de forma paginada e individual

---
+++ core/products/actions/get-product-by-id.action.ts
@@ -0,0 +1,15 @@
+import { API_URL, productsApi } from "@/core/api/productsApi";
+import { type Product } from "../interfaces/product.interface";
+
+export const getProductById = async (id: string): Promise<Product> => {
+  try {
+    const { data } = await productsApi.get<Product>(`/products/${id}`);
+
+    return {
+      ...data,
+      images: data.images.map((image) => `${API_URL}/files/products/${image}`),
+    };
+  } catch (error) {
+    throw new Error(`product with id ${id} not found`);
+  }
+};
---
+++ core/products/actions/get-products.action.ts
@@ -0,0 +1,21 @@
+import { API_URL, productsApi } from "@/core/api/productsApi";
+import { type Product } from "../interfaces/product.interface";
+
+export const getProducts = async (limit = 20, offset = 0) => {
+  try {
+    const { data } = await productsApi.get<Product[]>("/products", {
+      params: {
+        limit,
+        offset,
+      },
+    });
+    return data.map((product) => ({
+      ...product,
+      images: product.images.map(
+        (image) => `${API_URL}/files/products/${image}`
+      ),
+    }));
+  } catch (error) {
+    throw new Error("Unable to load products");
+  }
+};
